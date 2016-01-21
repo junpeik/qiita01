a8dc4a7edb5086f0a7d37118523fe21c0bda7038
Junpei <kusumoto.0513.jp@gmail.com>
2016-01-21 14:07:47 +0900

    投稿機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :set_user, only: [:show, :new]
+
   def index
     @items = Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item = @user.items.find(params[:id])
   end
+
+  def new
+    @item = @user.items.build
+  end
+
+  def create
+    @item = @user.items.buid(item_params)
+    if @item.save
+
+    else
+
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def item_params
+      params.require(:item).permit(:title, :body)
+    end
 end
